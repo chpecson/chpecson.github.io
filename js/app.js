3ee2c0f3d9002e1a8cd3dc0b2ae7e13b5bb2b3e5
chpecson <chpecson@gmail.com>
2020-04-23 10:49:40 +0800

    Optimization: fix lighthouse reports

--- js/app.js
+++ js/app.js
@@ -46,69 +46,17 @@
         });
     },
 
-    Invoza.prototype.initTesticarousel = function() {
-        $('#testi-carousel').owlCarousel({
-            items: 1,
-            loop:true,
-            margin:10,
-            nav:true,
-            responsive:{
-                576:{
-                    items:2
-                },
-     
-            }
-        });
-    },
-
-    Invoza.prototype.initCounter = function() {
-        // Counter Number
-        var a = 0;
-        $(window).scroll(function() {
-            var oTop = $('#counter').offset().top - window.innerHeight;
-            if (a == 0 && $(window).scrollTop() > oTop) {
-                $('.counter-value').each(function() {
-                    var $this = $(this),
-                        countTo = $this.attr('data-count');
-                    $({
-                        countNum: $this.text()
-                    }).animate({
-                            countNum: countTo
-                        },
-
-                        {
-
-                            duration: 2000,
-                            easing: 'swing',
-                            step: function() {
-                                $this.text(Math.floor(this.countNum));
-                            },
-                            complete: function() {
-                                $this.text(this.countNum);
-                                //alert('finished');
-                            }
-
-                        });
-                });
-                a = 1;
-            }
-        });
-
-    },
-
-
-    feather.replace()
 
     Invoza.prototype.init = function() {
+        feather.replace();
+
         this.initTypedJs();
         this.initStickyMenu();
         this.initSmoothLink();
         this.initScrollspy();
-        this.initTesticarousel();
-        this.initCounter();
     },
     //init
-    $.Invoza = new Invoza, $.Invoza.Constructor = Invoza
+    $.Invoza = new Invoza(), $.Invoza.Constructor = Invoza;
 }(window.jQuery),
 
 
